2f2400d2c707ba6b272d55ab6d827d56583a159d
Семенюк Василь <givilur@gmail.com>
2021-03-07 12:50:56 +0200

    Add project files.

---
+++ TeamworkSystem.WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeamworkSystem.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
