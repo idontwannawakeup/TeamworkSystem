9a5d82b7a716b97f9bcd4f6144008f96f20d41f6
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-01-06 12:48:22 +0200

    Transition to minimal api

--- TeamworkSystem.WebAPI/Program.cs
+++ TeamworkSystem.WebAPI/Program.cs
@@ -1,15 +1,141 @@
-namespace TeamworkSystem.WebAPI
-{
-    public class Program
-    {
-        public static void Main(string[] args) =>
-            CreateHostBuilder(args).Build().Run();
+using System.Text;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using TeamworkSystem.BusinessLogicLayer.Configurations;
+using TeamworkSystem.BusinessLogicLayer.Factories;
+using TeamworkSystem.BusinessLogicLayer.Interfaces;
+using TeamworkSystem.BusinessLogicLayer.Interfaces.Services;
+using TeamworkSystem.BusinessLogicLayer.Services;
+using TeamworkSystem.DataAccessLayer;
+using TeamworkSystem.DataAccessLayer.Data;
+using TeamworkSystem.DataAccessLayer.Data.Repositories;
+using TeamworkSystem.DataAccessLayer.Entities;
+using TeamworkSystem.DataAccessLayer.Interfaces;
+using TeamworkSystem.DataAccessLayer.Interfaces.Repositories;
+using TeamworkSystem.WebAPI.Middlewares;
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+services.AddDbContext<TeamworkSystemContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    options.UseSqlServer(connectionString);
+});
+
+services.AddTransient<IProjectsRepository, ProjectsRepository>();
+services.AddTransient<IRatingsRepository, RatingsRepository>();
+services.AddTransient<ITeamsRepository, TeamsRepository>();
+services.AddTransient<ITicketsRepository, TicketsRepository>();
+
+services.AddTransient<IUnitOfWork, UnitOfWork>();
+
+services.AddTransient<IIdentityService, IdentityService>();
+services.AddTransient<IPhotosService, PhotosService>();
+services.AddTransient<IProjectsService, ProjectsService>();
+services.AddTransient<IRatingsService, RatingsService>();
+services.AddTransient<ITeamsService, TeamsService>();
+services.AddTransient<ITicketsService, TicketsService>();
+services.AddTransient<IUsersService, UsersService>();
+
+services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+services.AddTransient<JwtTokenConfiguration>();
+services.AddTransient<IJwtSecurityTokenFactory, JwtSecurityTokenFactory>();
+
+services.AddTransient<IValidatorFactory, ServiceProviderValidatorFactory>();
+services.AddMvc(options => { options.EnableEndpointRouting = false; })
+        .AddFluentValidation(configuration =>
+        {
+            configuration.RegisterValidatorsFromAssemblies(
+                AppDomain.CurrentDomain.GetAssemblies());
+        });
+
+services.AddIdentityCore<User>()
+        .AddRoles<IdentityRole>()
+        .AddSignInManager<SignInManager<User>>()
+        .AddDefaultTokenProviders()
+        .AddEntityFrameworkStores<TeamworkSystemContext>();
+
+services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(builder.Configuration["JwtSecurityKey"])),
+                ClockSkew = TimeSpan.Zero,
+            };
+        });
+
+services.AddControllers();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "TeamworkSystem.WebAPI",
+        Version = "v1",
+    });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme.
+                                    Enter 'Bearer' [space] and then your token in the
+                                    text input below. Example: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
                 {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer",
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+            },
+            new List<string>()
+        },
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseMiddleware<ExceptionHandlerMiddleware>();
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+app.UseStaticFiles();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
