6c2059735fb7b968a78ad2bb8a3b8f2d7243a944
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-03-12 23:07:31 +0200

    Migrated presentation layer of core

---
+++ TeamworkSystem.Core.API/Controllers/TeamsController.cs
@@ -0,0 +1,169 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TeamworkSystem.Core.BusinessLogic.DTO.Requests;
+using TeamworkSystem.Core.BusinessLogic.DTO.Responses;
+using TeamworkSystem.Core.BusinessLogic.Interfaces.Services;
+using TeamworkSystem.Core.DataAccess.Parameters;
+using TeamworkSystem.Shared.Exceptions;
+using TeamworkSystem.Shared.Pagination;
+
+namespace TeamworkSystem.Core.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TeamsController : ControllerBase
+{
+    private readonly ITeamsService _teamsService;
+
+    public TeamsController(ITeamsService teamsService) => _teamsService = teamsService;
+
+    [HttpGet]
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult<PagedList<TeamResponse>>> GetAsync(
+        [FromQuery] TeamsParameters parameters)
+    {
+        try
+        {
+            var teams = await _teamsService.GetAsync(parameters);
+            Response.Headers.Add("X-Pagination", teams.SerializeMetadata());
+            return Ok(teams);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new { e.Message });
+        }
+    }
+
+    [HttpGet("{id}")]
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult<TeamResponse>> GetByIdAsync([FromRoute] int id) =>
+        Ok(await _teamsService.GetByIdAsync(id));
+
+    [HttpPost]
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult> InsertAsync([FromBody] TeamRequest request)
+    {
+        try
+        {
+            await _teamsService.InsertAsync(request);
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new { e.Message });
+        }
+    }
+
+    [HttpPut]
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult> UpdateAsync([FromBody] TeamRequest request)
+    {
+        try
+        {
+            await _teamsService.UpdateAsync(request);
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new { e.Message });
+        }
+    }
+
+    [HttpPost("avatar")]
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult> SetAvatarForTeamAsync([FromForm] TeamAvatarRequest request)
+    {
+        try
+        {
+            await _teamsService.SetAvatarForTeamAsync(request);
+            return Ok();
+        }
+        catch (EntityNotFoundException e)
+        {
+            return NotFound(new { e.Message });
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new { e.Message });
+        }
+    }
+
+    [HttpDelete("{id}")]
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult> DeleteAsync([FromRoute] int id)
+    {
+        try
+        {
+            await _teamsService.DeleteAsync(id);
+            return Ok();
+        }
+        catch (EntityNotFoundException e)
+        {
+            return NotFound(new { e.Message });
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new { e.Message });
+        }
+    }
+
+    [HttpPost("members")]
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult> AddMemberAsync([FromBody] TeamMemberRequest request)
+    {
+        try
+        {
+            await _teamsService.AddMemberAsync(request);
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new { e.Message });
+        }
+    }
+
+    [HttpDelete("members/{teamId}/{memberId}")]
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult> DeleteMemberAsync(
+        [FromRoute] int teamId,
+        [FromRoute] string memberId)
+    {
+        try
+        {
+            var request = new TeamMemberRequest { TeamId = teamId, UserId = memberId };
+            await _teamsService.DeleteMemberAsync(request);
+            return Ok();
+        }
+        catch (EntityNotFoundException e)
+        {
+            return NotFound(new { e.Message });
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new { e.Message });
+        }
+    }
+}
