448ad3b560c99faf83e383925d2bd1407fca873d
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-03-13 00:01:06 +0200

    Added dependency injection for social microservice

--- TeamworkSystem.Social.API/Program.cs
+++ TeamworkSystem.Social.API/Program.cs
@@ -1,23 +1,75 @@
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.OpenApi.Models;
+using TeamworkSystem.Social.BusinessLogic.DependencyInjection;
+using TeamworkSystem.Social.DataAccess.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
 
-// Add services to the container.
+services.AddDatabase(builder.Configuration);
+services.AddData();
+services.AddFilterFactories();
+services.AddServices();
+services.AddSeeding();
+services.AddValidation();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1",
+        new OpenApiInfo
+        {
+            Title = "TeamworkSystem.WebAPI",
+            Version = "v1"
+        });
+
+    c.AddSecurityDefinition("Bearer",
+        new OpenApiSecurityScheme
+        {
+            Description = @"JWT Authorization header using the Bearer scheme.
+                            Enter 'Bearer' [space] and then your token in the
+                            text input below. Example: 'Bearer 12345abcdef'",
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer"
+        });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseMiddleware<ExceptionHandlerMiddleware>();
+
 app.UseHttpsRedirection();
 
+// TODO: maybe it's required to add authentication
 app.UseAuthorization();
 
 app.MapControllers();
---
+++ TeamworkSystem.Social.BusinessLogic/DependencyInjection/ServicesExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TeamworkSystem.Social.BusinessLogic.Interfaces.Services;
+using TeamworkSystem.Social.BusinessLogic.Services;
+
+namespace TeamworkSystem.Social.BusinessLogic.DependencyInjection;
+
+public static class ServicesExtensions
+{
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services.AddTransient<IRatingsService, RatingsService>();
+        return services;
+    }
+}
