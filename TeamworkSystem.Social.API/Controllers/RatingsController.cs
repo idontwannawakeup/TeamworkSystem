9fddecb0d5010e799b8dfd965219eea306fdb06f
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-03-12 23:53:45 +0200

    Migrated presentation layer of social

---
+++ TeamworkSystem.Social.API/Controllers/RatingsController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TeamworkSystem.Shared.Pagination;
+using TeamworkSystem.Social.BusinessLogic.DTO.Requests;
+using TeamworkSystem.Social.BusinessLogic.DTO.Responses;
+using TeamworkSystem.Social.BusinessLogic.Interfaces.Services;
+using TeamworkSystem.Social.DataAccess.Parameters;
+
+namespace TeamworkSystem.Social.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class RatingsController : ControllerBase
+{
+    private readonly IRatingsService _ratingsService;
+
+    public RatingsController(IRatingsService ratingsService) => _ratingsService = ratingsService;
+
+    [HttpGet]
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult<PagedList<RatingResponse>>> GetAsync(
+        [FromQuery] RatingsParameters parameters)
+    {
+        var ratings = await _ratingsService.GetAsync(parameters);
+        Response.Headers.Add("X-Pagination", ratings.SerializeMetadata());
+        return Ok(ratings);
+    }
+
+    [HttpGet("{id}")]
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult<IEnumerable<RatingResponse>>> GetByIdAsync([FromRoute] int id) =>
+        Ok(await _ratingsService.GetByIdAsync(id));
+
+    [HttpPost]
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult> InsertAsync([FromBody] RatingRequest request)
+    {
+        try
+        {
+            await _ratingsService.InsertAsync(request);
+            return Ok();
+        }
+        catch (DbUpdateException)
+        {
+            return StatusCode(
+                StatusCodes.Status500InternalServerError,
+                new { Message = "Rating from you to this user already exists." });
+        }
+    }
+
+    [HttpPut]
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult> UpdateAsync([FromBody] RatingRequest request)
+    {
+        await _ratingsService.UpdateAsync(request);
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult> DeleteAsync([FromRoute] int id)
+    {
+        await _ratingsService.DeleteAsync(id);
+        return Ok();
+    }
+}
---
+++ TeamworkSystem.Social.API/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using TeamworkSystem.Shared.Exceptions;
+
+namespace TeamworkSystem.Social.API.Middlewares;
+
+public class ExceptionHandlerMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionHandlerMiddleware(RequestDelegate next) => _next = next;
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception e)
+        {
+            context.Response.StatusCode = e switch
+            {
+                EntityNotFoundException => (int) HttpStatusCode.NotFound,
+                _ => (int) HttpStatusCode.InternalServerError,
+            };
+
+            await context.Response.WriteAsJsonAsync(new { e.Message });
+        }
+    }
+}
