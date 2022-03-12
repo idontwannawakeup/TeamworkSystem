49eafd9d0243874df15a71bb9ea549b1351c3262
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-03-12 23:17:50 +0200

    Added dependency injection for core microservice

---
+++ TeamworkSystem.Core.DataAccess/DependencyInjection/SeedingExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TeamworkSystem.Core.DataAccess.Interfaces.Seeders;
+using TeamworkSystem.Core.DataAccess.Seeders;
+
+namespace TeamworkSystem.Core.DataAccess.DependencyInjection;
+
+public static class SeedingExtensions
+{
+    public static IServiceCollection AddSeeding(this IServiceCollection services)
+    {
+        services.AddTransient<IUserProfileSeeder, UserProfileSeeder>();
+        services.AddTransient<IProjectSeeder, ProjectSeeder>();
+        services.AddTransient<ITeamSeeder, TeamSeeder>();
+        services.AddTransient<ITeamsMembersSeeder, TeamsMembersSeeder>();
+        services.AddTransient<ITicketSeeder, TicketSeeder>();
+        return services;
+    }
+}
