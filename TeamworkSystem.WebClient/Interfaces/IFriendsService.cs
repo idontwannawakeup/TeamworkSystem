654f8151ee354845da21c60471bfc674d6f354da
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-05-15 00:39:54 +0300

    Implemented usage of new friends endpoints on blazor ui

---
+++ TeamworkSystem.WebClient/Interfaces/IFriendsService.cs
@@ -0,0 +1,16 @@
+﻿using TeamworkSystem.WebClient.Parameters;
+using TeamworkSystem.WebClient.ViewModels;
+
+namespace TeamworkSystem.WebClient.Interfaces;
+
+public interface IFriendsService
+{
+    Task<IEnumerable<UserViewModel>> GetFriendsAsync(Guid id);
+
+    Task<(IEnumerable<UserViewModel>, PaginationHeaderViewModel)>
+        GetFriendsWithPaginationHeaderAsync(Guid id, FriendsParameters parameters);
+
+    Task AddFriendsAsync(FriendsViewModel viewModel);
+
+    Task DeleteFriendsAsync(FriendsViewModel viewModel);
+}
---
+++ TeamworkSystem.WebClient/Parameters/FriendsParameters.cs
@@ -0,0 +1,6 @@
+﻿namespace TeamworkSystem.WebClient.Parameters;
+
+public class FriendsParameters : QueryStringParameters
+{
+    public string LastName { get; set; }
+}
