f4b69017c13ba4c73d3acef8e8b16798f66edf0b
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-05-14 15:45:37 +0300

    Added ocelot to project and configured it

--- TeamworkSystem.Gateway/Program.cs
+++ TeamworkSystem.Gateway/Program.cs
@@ -1,6 +1,15 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOcelot();
+builder.Configuration.AddJsonFile("ocelot.json");
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+app.MapControllers();
+await app.UseOcelot();
 
 app.Run();
