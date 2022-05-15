f02b8b909d5072f4a643fabd32d3fca6b79bbdea
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-05-15 19:31:47 +0300

    Implemented get teams query

---
+++ TeamworkSystem.WorkManagement.Application/Teams/Queries/GetTeams/GetTeamsQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using TeamworkSystem.Shared.Pagination;
+using TeamworkSystem.WorkManagement.Application.Common.Responses;
+using TeamworkSystem.WorkManagement.Domain.Parameters;
+
+namespace TeamworkSystem.WorkManagement.Application.Teams.Queries.GetTeams;
+
+public class GetTeamsQuery : IRequest<PagedList<TeamResponse>>
+{
+    public TeamsParameters Parameters { get; set; } = default!;
+}
