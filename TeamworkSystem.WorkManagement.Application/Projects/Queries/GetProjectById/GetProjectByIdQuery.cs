761dd14ed39078551d14ddcda87d45d8941c4eca
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-05-15 20:55:24 +0300

    Implemented get project by id query

---
+++ TeamworkSystem.WorkManagement.Application/Projects/Queries/GetProjectById/GetProjectByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using TeamworkSystem.WorkManagement.Application.Common.Responses;
+
+namespace TeamworkSystem.WorkManagement.Application.Projects.Queries.GetProjectById;
+
+public class GetProjectByIdQuery : IRequest<ProjectResponse>
+{
+    public Guid Id { get; set; }
+}
---
+++ TeamworkSystem.WorkManagement.Application/Projects/Queries/GetProjectById/GetProjectByIdQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using AutoMapper;
+using TeamworkSystem.WorkManagement.Application.Common.Responses;
+using TeamworkSystem.WorkManagement.Application.Interfaces;
+using TeamworkSystem.WorkManagement.Domain.Entities;
+
+namespace TeamworkSystem.WorkManagement.Application.Projects.Queries.GetProjectById;
+
+public class GetProjectByIdQueryHandler : IRequestHandler<GetProjectByIdQuery, ProjectResponse>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public GetProjectByIdQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    public async Task<ProjectResponse> Handle(
+        GetProjectByIdQuery request,
+        CancellationToken cancellationToken)
+    {
+        var project = await _unitOfWork.ProjectsRepository.GetCompleteEntityAsync(request.Id);
+        return _mapper.Map<Project, ProjectResponse>(project);
+    }
+}
