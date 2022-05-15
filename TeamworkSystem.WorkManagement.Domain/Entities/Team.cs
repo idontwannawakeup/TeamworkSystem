83f96dd6939ee18b0911d35c4ff8ba088774a3f2
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-05-15 17:30:51 +0300

    Added entities

---
+++ TeamworkSystem.WorkManagement.Domain/Entities/Team.cs
@@ -0,0 +1,16 @@
+﻿namespace TeamworkSystem.WorkManagement.Domain.Entities;
+
+public class Team
+{
+    public Guid Id { get; set; }
+    public Guid? LeaderId { get; set; }
+
+    public string Name { get; set; } = default!;
+    public string? Specialization { get; set; }
+    public string? About { get; set; }
+    public string? Avatar { get; set; }
+
+    public UserProfile? Leader { get; set; }
+    public List<Project> Projects { get; set; } = default!;
+    public List<UserProfile> Members { get; set; } = default!;
+}
