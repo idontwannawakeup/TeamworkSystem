448ea8fc4e6a73b5a0fbb13e25f22809629b0a82
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-05-15 19:31:38 +0300

    Implemented get user teams query

---
+++ TeamworkSystem.WorkManagement.Application/Teams/Queries/GetUserTeams/GetUserTeamsQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using TeamworkSystem.WorkManagement.Application.Common.Responses;
+
+namespace TeamworkSystem.WorkManagement.Application.Teams.Queries.GetUserTeams;
+
+public class GetUserTeamsQuery : IRequest<IEnumerable<TeamResponse>>
+{
+    public Guid UserId { get; set; }
+}
