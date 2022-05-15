ae0497115c0d1ebd0a1cb3b7ba59a098b1bb382a
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-05-15 18:05:32 +0300

    Moved persistence layer

---
+++ TeamworkSystem.WorkManagement.Persistence/Filters/Projects/TitleCriterion.cs
@@ -0,0 +1,18 @@
+using System.Linq.Expressions;
+using TeamworkSystem.Shared.Interfaces.Filters;
+using TeamworkSystem.WorkManagement.Domain.Entities;
+using TeamworkSystem.WorkManagement.Domain.Parameters;
+
+namespace TeamworkSystem.WorkManagement.Persistence.Filters.Projects;
+
+public class TitleCriterion : IFilterCriterion<Project>
+{
+    private readonly ProjectsParameters _parameters;
+
+    public TitleCriterion(ProjectsParameters parameters) => _parameters = parameters;
+
+    public bool Condition => !string.IsNullOrWhiteSpace(_parameters.Title);
+
+    public Expression<Func<Project, bool>> Expression =>
+        project => project.Title.Contains(_parameters.Title!);
+}
---
+++ TeamworkSystem.WorkManagement.Persistence/Filters/Tickets/ExecutorIdCriterion.cs
@@ -0,0 +1,19 @@
+using System.Linq.Expressions;
+using TeamworkSystem.Shared.Interfaces.Filters;
+using TeamworkSystem.WorkManagement.Domain.Entities;
+using TeamworkSystem.WorkManagement.Domain.Parameters;
+
+namespace TeamworkSystem.WorkManagement.Persistence.Filters.Tickets;
+
+public class ExecutorIdCriterion : IFilterCriterion<Ticket>
+{
+    private readonly TicketsParameters _parameters;
+
+    public ExecutorIdCriterion(TicketsParameters parameters) =>
+        _parameters = parameters;
+
+    public bool Condition => _parameters.ExecutorId is not null;
+
+    public Expression<Func<Ticket, bool>> Expression =>
+        ticket => ticket.ExecutorId == _parameters.ExecutorId;
+}
