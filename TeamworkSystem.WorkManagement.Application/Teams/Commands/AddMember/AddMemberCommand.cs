1552d757e04b9b5007cfa77c689d29d33e31e0d5
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-05-15 19:15:08 +0300

    Implemented add member command

---
+++ TeamworkSystem.WorkManagement.Application/Teams/Commands/AddMember/AddMemberCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace TeamworkSystem.WorkManagement.Application.Teams.Commands.AddMember;
+
+public class AddMemberCommand : IRequest
+{
+    public Guid UserId { get; set; }
+    public Guid TeamId { get; set; }
+}
