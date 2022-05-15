96b9fbe1d4934342f149834de88755f12700151c
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-05-15 19:31:14 +0300

    Added responses

---
+++ TeamworkSystem.WorkManagement.Application/Common/Responses/TeamResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace TeamworkSystem.WorkManagement.Application.Common.Responses;
+
+public class TeamResponse
+{
+    public Guid Id { get; set; }
+    public Guid? LeaderId { get; set; }
+
+    public string Name { get; set; } = default!;
+    public string? Specialization { get; set; }
+    public string? About { get; set; }
+    public string? Avatar { get; set; }
+
+    public UserResponse Leader { get; set; } = default!;
+}
