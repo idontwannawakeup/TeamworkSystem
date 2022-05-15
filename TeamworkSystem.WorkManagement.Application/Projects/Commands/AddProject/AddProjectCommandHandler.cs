87260c56db9008fe5d553db8538a24f58087bda0
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-05-15 20:54:54 +0300

    Implemented add project command

---
+++ TeamworkSystem.WorkManagement.Application/Projects/Commands/AddProject/AddProjectCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MediatR;
+using TeamworkSystem.WorkManagement.Application.Interfaces;
+using TeamworkSystem.WorkManagement.Domain.Entities;
+
+namespace TeamworkSystem.WorkManagement.Application.Projects.Commands.AddProject;
+
+public class AddProjectCommandHandler : IRequestHandler<AddProjectCommand>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public AddProjectCommandHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    public async Task<Unit> Handle(AddProjectCommand request, CancellationToken cancellationToken)
+    {
+        var project = _mapper.Map<AddProjectCommand, Project>(request);
+        await _unitOfWork.ProjectsRepository.InsertAsync(project);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+        return Unit.Value;
+    }
+}
