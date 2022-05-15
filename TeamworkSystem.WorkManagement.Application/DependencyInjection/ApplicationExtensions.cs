3a52c1096c2b54b252190d115e860555615f226b
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-05-15 19:04:17 +0300

    Implemented set team avatar command

--- TeamworkSystem.WorkManagement.Application/DependencyInjection/ApplicationExtensions.cs
+++ TeamworkSystem.WorkManagement.Application/DependencyInjection/ApplicationExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using System.Reflection;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using TeamworkSystem.WorkManagement.Application.Common.Storages;
+using TeamworkSystem.WorkManagement.Application.Interfaces.Storages;
 
 namespace TeamworkSystem.WorkManagement.Application.DependencyInjection;
 
@@ -9,6 +11,7 @@ public static class ApplicationExtensions
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(Assembly.GetExecutingAssembly());
+        services.AddTransient<IPhotoStorage, PhotoStorage>();
         return services;
     }
 }
