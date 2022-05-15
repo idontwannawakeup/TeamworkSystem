76b1664532841dcd2e186ac80ca1316afa5f04db
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-05-15 20:55:09 +0300

    Implemented delete project command

---
+++ TeamworkSystem.WorkManagement.Application/Projects/Commands/DeleteProject/DeleteProjectCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace TeamworkSystem.WorkManagement.Application.Projects.Commands.DeleteProject;
+
+public class DeleteProjectCommand : IRequest
+{
+    public Guid Id { get; set; }
+}
---
+++ TeamworkSystem.WorkManagement.Application/Projects/Commands/DeleteProject/DeleteProjectCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MediatR;
+using TeamworkSystem.WorkManagement.Application.Interfaces;
+
+namespace TeamworkSystem.WorkManagement.Application.Projects.Commands.DeleteProject;
+
+public class DeleteProjectCommandHandler : IRequestHandler<DeleteProjectCommand>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public DeleteProjectCommandHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    public async Task<Unit> Handle(
+        DeleteProjectCommand request,
+        CancellationToken cancellationToken)
+    {
+        await _unitOfWork.ProjectsRepository.DeleteAsync(request.Id);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+        return Unit.Value;
+    }
+}
