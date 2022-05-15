9ddaf9304b915cbaac058cde31b727b4b1ce6ab0
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-05-15 18:54:51 +0300

    Implemented update team command

---
+++ TeamworkSystem.WorkManagement.Application/Common/Responses/ErrorResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace TeamworkSystem.WorkManagement.Application.Common.Responses;
+
+public class ErrorResponse
+{
+    public ErrorResponse()
+    {
+    }
+
+    public ErrorResponse(string error) => Errors = new List<string> { error };
+
+    public List<string> Errors { get; } = new();
+}
