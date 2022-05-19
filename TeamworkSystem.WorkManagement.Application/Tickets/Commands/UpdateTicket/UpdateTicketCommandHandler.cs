8176d1983e377c5ab53c1cea70340a4041c636b7
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-05-19 22:16:40 +0300

    Implemented update ticket command

---
+++ TeamworkSystem.WorkManagement.Application/Tickets/Commands/UpdateTicket/UpdateTicketCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using TeamworkSystem.WorkManagement.Application.Interfaces;
+
+namespace TeamworkSystem.WorkManagement.Application.Tickets.Commands.UpdateTicket;
+
+public class UpdateTicketCommandHandler : IRequestHandler<UpdateTicketCommand>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public UpdateTicketCommandHandler(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+
+    public async Task<Unit> Handle(UpdateTicketCommand request, CancellationToken cancellationToken)
+    {
+        var ticket = await _unitOfWork.TicketsRepository.GetByIdAsync(request.Id);
+        ticket.ProjectId = request.ProjectId;
+        ticket.ExecutorId = request.ExecutorId;
+        ticket.Title = request.Title;
+        ticket.Type = request.Type;
+        ticket.Description = request.Description;
+        ticket.Status = request.Status;
+        ticket.Priority = request.Priority;
+        ticket.Deadline = request.Deadline;
+        await _unitOfWork.TicketsRepository.UpdateAsync(ticket);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+        return Unit.Value;
+    }
+}
