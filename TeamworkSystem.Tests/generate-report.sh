11e262bdfa294b0e292fb640254916d48cb75209
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-06-05 13:41:03 +0300

    Added coverage scripts and nuget

---
+++ TeamworkSystem.Tests/generate-report.sh
@@ -0,0 +1,1 @@
+dotnet $HOME/.nuget/packages/reportgenerator/5.1.9/tools/net6.0/ReportGenerator.dll "-reports:TestResults/$1/coverage.cobertura.xml" "-targetdir:TestResults/$1/coveragereport" -reporttypes:Html
