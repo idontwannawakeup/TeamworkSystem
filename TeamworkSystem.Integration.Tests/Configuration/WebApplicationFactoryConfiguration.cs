fc70024edf6ad794923bb6650cadaf8fc8d7fb64
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-06-05 17:08:15 +0300

    Configured web app factory

---
+++ TeamworkSystem.Integration.Tests/Configuration/WebApplicationFactoryConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TeamworkSystem.DataAccessLayer;
+
+namespace TeamworkSystem.Integration.Tests.Configuration;
+
+public class WebApplicationFactoryConfiguration
+{
+    public static WebApplicationFactory<Program> GetConfiguredApplication()
+    {
+        return new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureServices(services =>
+            {
+                var context = services.Single(d =>
+                    d.ServiceType == typeof(DbContextOptions<TeamworkSystemContext>));
+
+                services.Remove(context);
+                services.AddDbContext<TeamworkSystemContext>(options =>
+                {
+                    options.UseInMemoryDatabase("TeamworkSystemInMemoryDb");
+                });
+            });
+        });
+    }
+}
