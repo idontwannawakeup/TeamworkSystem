6bccc4cb52ea4ede46193f33cbde2dd4f0b5b18f
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-06-07 09:28:52 +0300

    Added more integration tests

---
+++ TeamworkSystem.Integration.Tests/Tests/TicketsTests.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using TeamworkSystem.BusinessLogicLayer.DTO.Responses;
+using TeamworkSystem.DataAccessLayer;
+using TeamworkSystem.DataAccessLayer.Pagination;
+using TeamworkSystem.Integration.Tests.Configuration;
+
+namespace TeamworkSystem.Integration.Tests.Tests;
+
+public class TicketsTests
+{
+    private static readonly JsonSerializerOptions SerializerOptions = new JsonSerializerOptions
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+    };
+
+    private WebApplicationFactory<Program> _webApplicationFactory = default!;
+    private HttpClient _client = default!;
+
+    [SetUp]
+    public async Task Initialize()
+    {
+        _webApplicationFactory = WebApplicationFactoryConfiguration.GetConfiguredApplication();
+        _client = _webApplicationFactory.CreateClient();
+
+        await using var scope = _webApplicationFactory.Services.CreateAsyncScope();
+        var context = scope.ServiceProvider.GetRequiredService<TeamworkSystemContext>();
+        await context.Database.EnsureCreatedAsync();
+    }
+
+    [TearDown]
+    public async Task Finish()
+    {
+        await using var scope = _webApplicationFactory.Services.CreateAsyncScope();
+        var context = scope.ServiceProvider.GetRequiredService<TeamworkSystemContext>();
+        await context.Database.EnsureDeletedAsync();
+        await context.DisposeAsync();
+    }
+
+    [TestCase]
+    public async Task GetTickets_WhenCalledWithValidToken_ShouldReturnOk()
+    {
+        const string identityEndpoint = "api/Identity/signIn";
+        const string endpoint = "api/Tickets";
+        const HttpStatusCode expectedStatusCode = HttpStatusCode.OK;
+        var expectedTickets = new List<TicketResponse>
+        {
+            new TicketResponse
+            {
+                Id = 1,
+                ProjectId = 1,
+                ExecutorId = "61dfb9e3-1c27-424a-9963-9586ca110220",
+                Title = "Fix bug",
+                Description = "There's unknown bug. Just fix it.",
+                Type = "Epic",
+                Status = "Backlog",
+                Priority = "Medium",
+                CreationTime = DateTime.Parse("1/6/2022 1:57:03 PM")
+            }
+        };
+        var expectedTicket = expectedTickets.First();
+
+        var signInResult = await _client.PostAsJsonAsync(
+            identityEndpoint,
+            new { UserName = "User1", Password = "User%password1" },
+            SerializerOptions);
+
+        signInResult.IsSuccessStatusCode.Should().Be(true);
+
+        var jwt = await signInResult.Content.ReadFromJsonAsync<JwtResponse>(SerializerOptions);
+        _client.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Bearer", jwt?.Token);
+
+        var result = await _client.GetAsync(endpoint);
+        var actualStatusCode = result.StatusCode;
+
+        result.IsSuccessStatusCode.Should().Be(true);
+        actualStatusCode.Should().Be(expectedStatusCode);
+
+        var actualTickets =
+            await result.Content.ReadFromJsonAsync<List<TicketResponse>>(SerializerOptions);
+        actualTickets?.Count.Should().Be(expectedTickets.Count);
+        var actualTicket = actualTickets?.First();
+        actualTicket?.Id.Should().Be(expectedTicket.Id);
+        actualTicket?.Title.Should().Be(expectedTicket.Title);
+    }
+
+    [TestCase]
+    public async Task GetTickets_WhenCalledWithoutToken_ShouldReturnUnauthorized()
+    {
+        const string endpoint = "api/Tickets";
+        const HttpStatusCode expectedStatusCode = HttpStatusCode.Unauthorized;
+
+        var result = await _client.GetAsync(endpoint);
+        var actualStatusCode = result.StatusCode;
+
+        result.IsSuccessStatusCode.Should().NotBe(true);
+        actualStatusCode.Should().Be(expectedStatusCode);
+    }
+
+    [TestCase]
+    public async Task GetTicketById_WhenCalledWithValidToken_ShouldReturnOk()
+    {
+        const string identityEndpoint = "api/Identity/signIn";
+        const string endpoint = "api/Tickets/1";
+        const HttpStatusCode expectedStatusCode = HttpStatusCode.OK;
+        var expectedTicket = new TicketResponse
+        {
+            Id = 1,
+            ProjectId = 1,
+            ExecutorId = "61dfb9e3-1c27-424a-9963-9586ca110220",
+            Title = "Fix bug",
+            Description = "There's unknown bug. Just fix it.",
+            Type = "Epic",
+            Status = "Backlog",
+            Priority = "Medium",
+            CreationTime = DateTime.Parse("1/6/2022 1:57:03 PM")
+        };
+
+        var signInResult = await _client.PostAsJsonAsync(
+            identityEndpoint,
+            new { UserName = "User1", Password = "User%password1" },
+            SerializerOptions);
+
+        signInResult.IsSuccessStatusCode.Should().Be(true);
+
+        var jwt = await signInResult.Content.ReadFromJsonAsync<JwtResponse>(SerializerOptions);
+        _client.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Bearer", jwt?.Token);
+
+        var result = await _client.GetAsync(endpoint);
+        var actualStatusCode = result.StatusCode;
+
+        result.IsSuccessStatusCode.Should().Be(true);
+        actualStatusCode.Should().Be(expectedStatusCode);
+
+        var actualTicket = await result.Content.ReadFromJsonAsync<TicketResponse>(SerializerOptions);
+        actualTicket?.Id.Should().Be(expectedTicket.Id);
+        actualTicket?.Title.Should().Be(expectedTicket.Title);
+    }
+
+    [TestCase]
+    public async Task GetTicketById_WhenCalledWithoutToken_ShouldReturnUnauthorized()
+    {
+        const string endpoint = "api/Tickets/1";
+        const HttpStatusCode expectedStatusCode = HttpStatusCode.Unauthorized;
+
+        var result = await _client.GetAsync(endpoint);
+        var actualStatusCode = result.StatusCode;
+
+        result.IsSuccessStatusCode.Should().NotBe(true);
+        actualStatusCode.Should().Be(expectedStatusCode);
+    }
+}
