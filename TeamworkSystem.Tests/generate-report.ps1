3d8b8a01a6ad1e2a5cc43bda55afaacf0481aab4
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-06-07 09:13:30 +0300

    Added powershell script

---
+++ TeamworkSystem.Tests/generate-report.ps1
@@ -0,0 +1,2 @@
+﻿$id=$args[0]
+dotnet $env:USERPROFILE/.nuget/packages/reportgenerator/5.1.9/tools/net6.0/ReportGenerator.dll "-reports:TestResults/$id/coverage.cobertura.xml" "-targetdir:TestResults/$id/coveragereport" -reporttypes:Html
