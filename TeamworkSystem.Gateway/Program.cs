004e5b408497ea31feee5ac7d8272b4bfd11fb4f
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-06-12 14:52:24 +0300

    Implemented caching for friends endpoint

--- TeamworkSystem.Gateway/Program.cs
+++ TeamworkSystem.Gateway/Program.cs
@@ -1,9 +1,14 @@
+using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOcelot();
+builder.Services.AddOcelot().AddCacheManager(x =>
+{
+    x.WithDictionaryHandle();
+});
+
 builder.Configuration.AddJsonFile("ocelot.json", false, reloadOnChange: true);
 
 var app = builder.Build();
