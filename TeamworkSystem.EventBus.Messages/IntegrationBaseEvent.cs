147e6c847b05c0cfb9227446abe189789bcc8b31
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-06-12 19:25:04 +0300

    Added EventBus.Messages project

---
+++ TeamworkSystem.EventBus.Messages/IntegrationBaseEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace TeamworkSystem.EventBus.Messages;
+
+public class IntegrationBaseEvent
+{
+    public Guid Id { get; set; }
+    public DateTime TriggeredAt { get; } = DateTime.UtcNow;
+}
---
+++ TeamworkSystem.EventBus.Messages/UserCreatedEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace TeamworkSystem.EventBus.Messages;
+
+public class UserCreatedEvent : IntegrationBaseEvent
+{
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;
+    public string? Profession { get; set; }
+    public string? Specialization { get; set; }
+    public string? Avatar { get; set; }
+}
