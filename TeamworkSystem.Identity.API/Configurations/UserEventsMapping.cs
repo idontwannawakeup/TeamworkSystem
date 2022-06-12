7d7a73db2d8b08114469fffe8ccc63938f244a7d
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-06-12 19:37:32 +0300

    Added event publishing and mapping

---
+++ TeamworkSystem.Identity.API/Configurations/UserEventsMapping.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using TeamworkSystem.EventBus.Messages;
+using TeamworkSystem.Identity.BusinessLogic.DTO.Requests;
+
+namespace TeamworkSystem.Identity.API.Configurations;
+
+public class UserEventsMapping : Profile
+{
+    public UserEventsMapping()
+    {
+        CreateMap<UserSignUpRequest, UserCreatedEvent>();
+    }
+}
