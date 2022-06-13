e17dcdb0643958e9f5d42dfd1ead57ee39215826
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-06-13 23:39:47 +0300

    Implemented grpc client for recent projects

--- TeamworkSystem.Content.Application/Recent/Queries/GetRecentProjects/GetRecentProjectsQueryHandler.cs
+++ TeamworkSystem.Content.Application/Recent/Queries/GetRecentProjects/GetRecentProjectsQueryHandler.cs
@@ -1,15 +1,44 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using Grpc.Net.Client;
+using MediatR;
 using TeamworkSystem.Content.Application.Common.Responses;
+using TeamworkSystem.Content.Application.Grpc.Definitions;
+using TeamworkSystem.Content.Application.Interfaces;
+using TeamworkSystem.Content.Domain.Enums;
 
 namespace TeamworkSystem.Content.Application.Recent.Queries.GetRecentProjects;
 
 public class GetRecentProjectsQueryHandler
     : IRequestHandler<GetRecentProjectsQuery, IEnumerable<ProjectResponse>>
 {
-    public Task<IEnumerable<ProjectResponse>> Handle(
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public GetRecentProjectsQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    public async Task<IEnumerable<ProjectResponse>> Handle(
         GetRecentProjectsQuery request,
         CancellationToken cancellationToken)
     {
-        return Task.FromResult(Array.Empty<ProjectResponse>() as IEnumerable<ProjectResponse>);
+        var recent = await _unitOfWork.RecentRequestRepository.GetAsync(
+            request.UserId,
+            RecentRequestEntityType.Project);
+
+        var grpcRequest = new GetRecentProjectsRequest
+        {
+            Ids = { recent.Select(r => r.RequestedEntityId.ToString()) }
+        };
+
+        using var channel = GrpcChannel.ForAddress("https://localhost:7077");
+        var client = new RecentRequestsService.RecentRequestsServiceClient(channel);
+        var response = await client.GetRecentProjectsAsync(
+            grpcRequest,
+            cancellationToken: cancellationToken);
+
+        return _mapper.Map<IEnumerable<ProjectResponse>>(response.Projects);
     }
 }
