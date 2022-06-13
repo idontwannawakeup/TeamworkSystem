7d2b8cbe9c968a349f69cdf741bcbbb989eb6f55
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-06-13 12:48:54 +0300

    Added notification templates

---
+++ TeamworkSystem.Content.Persistence/Configurations/NotificationTemplateConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TeamworkSystem.Content.Domain.Entities;
+using TeamworkSystem.Content.Domain.Enums;
+
+namespace TeamworkSystem.Content.Persistence.Configurations;
+
+public class NotificationTemplateConfiguration : IEntityTypeConfiguration<NotificationTemplate>
+{
+    public void Configure(EntityTypeBuilder<NotificationTemplate> builder)
+    {
+        builder.HasData(
+            new NotificationTemplate
+            {
+                Id = NotificationType.TicketAssigned,
+                Title = "New ticket",
+                Message = "{FullName}, you were assigned to ticket: {TicketTitle}"
+            },
+            new NotificationTemplate
+            {
+                Id = NotificationType.TicketDeadlineExpiration,
+                Title = "Deadline reminder",
+                Message = "{FullName}, deadline for assigned ticket is soon: {TicketTitle}, {TicketDeadline}"
+            },
+            new NotificationTemplate
+            {
+                Id = NotificationType.FriendsRequest,
+                Title = "New friend",
+                Message = "{FullName}, you have new friend request!"
+            },
+            new NotificationTemplate
+            {
+                Id = NotificationType.AssignedTicketDescriptionChanged,
+                Title = "Ticket description changed",
+                Message = "{FullName}, description of ticket changed: {TicketTitle}"
+            });
+    }
+}
