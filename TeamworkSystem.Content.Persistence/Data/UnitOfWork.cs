bad930229c432c554bbdbd184eb74f125f74052c
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-06-13 13:34:03 +0300

    Implemented UnitOfWork

---
+++ TeamworkSystem.Content.Persistence/Data/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using TeamworkSystem.Content.Application;
+using TeamworkSystem.Content.Application.Interfaces.Repositories;
+
+namespace TeamworkSystem.Content.Persistence.Data;
+
+public class UnitOfWork : IUnitOfWork
+{
+    protected readonly ContentDbContext DatabaseContext;
+
+    public UnitOfWork(
+        ContentDbContext databaseContext,
+        INotificationRepository notificationRepository,
+        INotificationTemplateRepository notificationTemplateRepository,
+        IRecentRequestRepository recentRequestRepository)
+    {
+        DatabaseContext = databaseContext;
+        NotificationRepository = notificationRepository;
+        NotificationTemplateRepository = notificationTemplateRepository;
+        RecentRequestRepository = recentRequestRepository;
+    }
+
+    public INotificationRepository NotificationRepository { get; }
+    public INotificationTemplateRepository NotificationTemplateRepository { get; }
+    public IRecentRequestRepository RecentRequestRepository { get; }
+
+    public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default) =>
+        await DatabaseContext.SaveChangesAsync(cancellationToken);
+}
