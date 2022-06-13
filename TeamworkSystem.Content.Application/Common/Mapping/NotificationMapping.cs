eed25f580b099e1d11628554cfea6e7fb43e28fd
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-06-13 13:50:29 +0300

    Implemented SendNotificationCommand

---
+++ TeamworkSystem.Content.Application/Common/Mapping/NotificationMapping.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using TeamworkSystem.Content.Application.Notifications.Commands.SendNotification;
+using TeamworkSystem.Content.Domain.Entities;
+
+namespace TeamworkSystem.Content.Application.Common.Mapping;
+
+public class NotificationMapping : Profile
+{
+    public NotificationMapping()
+    {
+        CreateMap<SendNotificationCommand, Notification>();
+    }
+}
