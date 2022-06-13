79e7853b6fc1b3b5b5628b510b76f7d751211ef5
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-06-13 13:57:12 +0300

    Implemented GetTopRecentNotificationsQuery

---
+++ TeamworkSystem.Content.Application/Notifications/Queries/GetTopRecentNotifications/GetTopRecentNotificationsQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MediatR;
+using TeamworkSystem.Content.Application.Common.Responses;
+using TeamworkSystem.Content.Application.Interfaces;
+
+namespace TeamworkSystem.Content.Application.Notifications.Queries.GetTopRecentNotifications;
+
+public class GetTopRecentNotificationsQueryHandler
+    : IRequestHandler<GetTopRecentNotificationsQuery, IEnumerable<NotificationResponse>>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public GetTopRecentNotificationsQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    public async Task<IEnumerable<NotificationResponse>> Handle(
+        GetTopRecentNotificationsQuery request,
+        CancellationToken cancellationToken)
+    {
+        var notifications = await _unitOfWork.NotificationRepository.GetTopRecentAsync(request.UserId);
+        return _mapper.Map<IEnumerable<NotificationResponse>>(notifications);
+    }
+}
