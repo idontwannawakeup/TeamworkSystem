59e4a7adb5cccf0b5ad0b6fb69a1d4e82dd68798
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-06-14 13:46:59 +0300

    Implemented redis example

--- TeamworkSystem.Content.API/Controllers/RecentController.cs
+++ TeamworkSystem.Content.API/Controllers/RecentController.cs
@@ -1,6 +1,10 @@
-﻿using MediatR;
+﻿using System.Text;
+using System.Text.Json;
+using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using TeamworkSystem.Content.Application.Common.Responses;
 using TeamworkSystem.Content.Application.Recent.Queries.GetRecentProjects;
 using TeamworkSystem.Content.Application.Recent.Queries.GetRecentTeams;
 using TeamworkSystem.Content.Application.Recent.Queries.GetRecentTickets;
@@ -12,10 +16,12 @@ namespace TeamworkSystem.Content.API.Controllers;
 public class RecentController : ControllerBase
 {
     private readonly IMediator _mediator;
+    private readonly IDistributedCache _cache;
 
-    public RecentController(IMediator mediator)
+    public RecentController(IMediator mediator, IDistributedCache cache)
     {
         _mediator = mediator;
+        _cache = cache;
     }
 
     [HttpGet("projects/{id:guid}")]
@@ -25,8 +31,18 @@ public class RecentController : ControllerBase
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> GetRecentProjectsAsync([FromRoute] Guid id)
     {
+        var cachedResponse = await _cache.GetStringAsync($"{id}-{0}");
+        if (cachedResponse is not null)
+        {
+            return Ok(JsonSerializer.Deserialize<IEnumerable<ProjectResponse>>(cachedResponse));
+        }
+
         var query = new GetRecentProjectsQuery { UserId = id };
         var projects = await _mediator.Send(query);
+        await _cache.SetStringAsync(
+            $"{id}-{0}",
+            JsonSerializer.Serialize(projects));
+
         return Ok(projects);
     }
 
