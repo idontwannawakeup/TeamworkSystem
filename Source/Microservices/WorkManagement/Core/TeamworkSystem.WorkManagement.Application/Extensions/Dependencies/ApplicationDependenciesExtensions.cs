80b11da1c186c22e9969017e9fa1fc6e16f91fd4
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2022-09-24 21:12:25 +0300

    [refactoring] WorkManagement cleanup
    Removed unused services in WorkManagement microservices

--- Source/Microservices/WorkManagement/Core/TeamworkSystem.WorkManagement.Application/Extensions/Dependencies/ApplicationDependenciesExtensions.cs
+++ Source/Microservices/WorkManagement/Core/TeamworkSystem.WorkManagement.Application/Extensions/Dependencies/ApplicationDependenciesExtensions.cs
@@ -3,8 +3,6 @@ using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using TeamworkSystem.WorkManagement.Application.Common.Storages;
 using TeamworkSystem.WorkManagement.Application.Interfaces.Data.Storages;
-using TeamworkSystem.WorkManagement.Application.Interfaces.Services;
-using TeamworkSystem.WorkManagement.Application.Services;
 
 namespace TeamworkSystem.WorkManagement.Application.Extensions.Dependencies;
 
@@ -14,10 +12,6 @@ public static class ApplicationDependenciesExtensions
     {
         services.AddMediatR(Assembly.GetExecutingAssembly());
         services.AddTransient<IPhotoStorage, PhotoStorage>();
-
-        services.AddTransient<IProjectsService, ProjectsService>();
-        services.AddTransient<ITicketsService, TicketsService>();
-
         return services;
     }
 }
